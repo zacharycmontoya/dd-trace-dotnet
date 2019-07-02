94cfc7cbaa4adf4ed09d69e0d6dbca5e28e8d5f7
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-07-02 09:39:45 -0400

    add repro app with Idendity and EF

---
+++ samples-aspnet/Identity-Owin-EntityFramework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Identity_Owin_EntityFramework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ samples-aspnet/Identity-Owin-EntityFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity_Owin_EntityFramework.Startup))]
+namespace Identity_Owin_EntityFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
