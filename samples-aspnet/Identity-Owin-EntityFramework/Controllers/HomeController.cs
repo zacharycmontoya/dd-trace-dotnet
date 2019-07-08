6f68d370d579a140f63c4d1d57687234ad9ccaf6
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-07-08 11:15:22 -0400

    wip

--- samples-aspnet/Identity-Owin-EntityFramework/Controllers/HomeController.cs
+++ samples-aspnet/Identity-Owin-EntityFramework/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Datadog.Trace.ClrProfiler;
+using Identity_Owin_EntityFramework.Models;
 
 namespace Identity_Owin_EntityFramework.Controllers
 {
@@ -25,6 +29,16 @@ namespace Identity_Owin_EntityFramework.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public async Task<ActionResult> UsersAsync()
+        {
+            var attached = Instrumentation.ProfilerAttached;
+
+            var context = new ApplicationDbContext();
+            var users = await context.Users.ToListAsync();
+
+            return View("About");
         }
     }
 }
