9b56beeed31fee8618f26eddf800479d3eba59e6
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-07-09 14:03:09 -0400

    add new sample project

---
+++ samples/Samples.CustomInstrumentation/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Samples.CustomInstrumentation
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Method1("message", 5);
+        }
+
+        public static void Method1(string s, int i)
+        {
+        }
+    }
+}
