baf8aaf72e646e54854dc024e0a9fe610ed07e83
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-07-15 17:08:27 -0400

    create array with argument values

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/CustomIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/CustomIntegration.cs
@@ -13,10 +13,22 @@ namespace Datadog.Trace.ClrProfiler.Integrations
             TargetSignatureTypes = new[] { ClrNames.Void, ClrNames.String })]
         public static object Wrapper(__arglist)
         {
-            var iterator = new ArgIterator(__arglist);
-            int count = iterator.GetRemainingCount();
+            var args = new ArgIterator(__arglist);
+            var values = new object[args.GetRemainingCount()];
 
+            for (int x = 0; x < values.Length; x++)
+            {
+                values[x] = TypedReference.ToObject(args.GetNextArg());
+            }
+
+            // Scope scope = Start(values);
+            // TODO: call original method
             return null;
+        }
+
+        public static Scope Start(object[] args)
+        {
+            return Tracer.Instance.StartActive("TODO");
         }
     }
 }
